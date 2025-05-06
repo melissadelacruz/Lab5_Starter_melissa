98c3a8eb108496114f68a555dfcf7353a1ddebb4
Melissa De La Cruz <mdelacruz5445@gmail.com>
2025-05-06 00:37:18 -0700

    added speech synthesis code in explore.js

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Lab 5 - Starter
 Melissa De La Cruz
+
+**Part 2:**
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+        No, I would not use a unit test to test the message feature of message application since the "message" feature consists of multiple parts and we want to make small tests that test each part individually.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+        Yes, because we can make a rule such as no more than 80 characters and create tests for that and see if it behaves the way that it should such as a message with 81 characters is blocked.
+
+   
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,47 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+  const textInput = document.getElementById('text-to-speak');
+  const faceImage = document.querySelector('img');
+
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    voiceSelect.innerHTML = ''; // clear existing
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = index;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  // Load voices (some browsers need the 'voiceschanged' event)
+  populateVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  talkButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoice = voices[voiceSelect.value];
+    utterance.voice = selectedVoice;
+
+    // While speaking: open mouth
+    utterance.onstart = () => {
+      faceImage.src = 'assets/images/smiling-open.png';
+    };
+
+    // When done: close mouth
+    utterance.onend = () => {
+      faceImage.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utterance);
+  });
 }
